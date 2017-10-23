a913dc0dd49d591fb4e1b922410d6a4fcdd26026
jwen2 <31702182+jwen2@users.noreply.github.com>
2017-10-23 14:31:12 -0400

    Added Time and Pause Botton on Bottom Right

--- UI_Skeleton.py
+++ UI_Skeleton.py
@@ -1,14 +1,16 @@
-""" John Wen"""
+""" John Wen
+Time runs in milliseconds, (1000 = 1 second)
+"""
 
 import pygame
 import os
 
-# class powerupbox(object):
-#     def __init__(self, x0, x1, y0, y1):
-#         self.x0 = x0
-#         self.x1 = x1
-#         self.y0 = y0
-#         self.y1 = y1
+class powerupbox(object):
+    def __init__(self, x0, x1, y0, y1):
+        self.x0 = x0
+        self.x1 = x1
+        self.y0 = y0
+        self.y1 = y1
 
 pygame.init()
 
@@ -16,10 +18,23 @@ background_color = (255,255,255)
 RED = (255,0,0)
 width, height = 640, 480
 
+"""Bunch of variables"""
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Plague Simulation')
 screen.fill(background_color)
-RECT1 = pygame.Rect(600,440,40,40)
+
+UpgradeMenu = pygame.Rect(600,440,40,40)
+Timelocation = pygame.Rect(400,0,600,40)
+Populationlocation = pygame.Rect(0,440,300,400)
+"""Timer"""
+Time = 0
+ActiveTime = True
+font = pygame.font.SysFont('Consolas', 30)
+
+
+clock = pygame.time.Clock()
+
+
 
 running = True
 while running:
@@ -29,20 +44,29 @@ while running:
     """ circles represent classes for now"""
     pygame.draw.circle(screen, RED, (300,200),38)
     pygame.draw.circle(screen, (0,255,0), (150,250),36)
-    pygame.draw.circle(screen, (0,0,255), (200,450),30)
+    pygame.draw.circle(screen, (0,0,255), (200,350),30)
     """ Black box will be sending you to upgrade screen"""
-    pygame.draw.rect(screen, (0,0,0), RECT1)
-    """Red Rectangle will display time"""
-    pygame.draw.rect(screen, (RED), [300,0,600,40])
+    pygame.draw.rect(screen, (0,0,0), UpgradeMenu)
+    """Time Display"""
+    pygame.draw.rect(screen, (0,0,0), Timelocation)
+    """Country Population Display"""
+    pygame.draw.rect(screen, (0,0,0), Populationlocation)
+    """timer display"""
+    screen.blit(font.render(str(pygame.time.get_ticks()/1000), True, (255, 255, 255)), (400, 0))
+
     pygame.display.update()
 
+
     if event.type == pygame.MOUSEBUTTONDOWN:
-            click = RECT1.collidepoint(pygame.mouse.get_pos())
+            if UpgradeMenu.collidepoint(pygame.mouse.get_pos()):
+                if pygame.time.get_ticks() > (Time + 1000):
+                    Time = pygame.time.get_ticks()
+                    if ActiveTime == True:
+                        pygame.time.wait(2000)
+                        Time - 1001
+                        ActiveTime = False
+                    else:
+                        ActiveTime = True
 
-            if click == 1:
-                print ('CLICKED!')
-
-            click2 =
-
-
+                """ Compare last time clicked"""
 pygame.quit()
