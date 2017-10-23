086c735075800da53c85236f5e028b8437c7d6d4
jwen2 <31702182+jwen2@users.noreply.github.com>
2017-10-23 03:00:23 -0400

    Added Click Interaction

--- UI_Skeleton.py
+++ UI_Skeleton.py
@@ -3,6 +3,13 @@
 import pygame
 import os
 
+# class powerupbox(object):
+#     def __init__(self, x0, x1, y0, y1):
+#         self.x0 = x0
+#         self.x1 = x1
+#         self.y0 = y0
+#         self.y1 = y1
+
 pygame.init()
 
 background_color = (255,255,255)
@@ -12,6 +19,7 @@ width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Plague Simulation')
 screen.fill(background_color)
+RECT1 = pygame.Rect(600,440,40,40)
 
 running = True
 while running:
@@ -23,9 +31,18 @@ while running:
     pygame.draw.circle(screen, (0,255,0), (150,250),36)
     pygame.draw.circle(screen, (0,0,255), (200,450),30)
     """ Black box will be sending you to upgrade screen"""
-    pygame.draw.rect(screen, (0,0,0), [600,440,40,40])
+    pygame.draw.rect(screen, (0,0,0), RECT1)
     """Red Rectangle will display time"""
     pygame.draw.rect(screen, (RED), [300,0,600,40])
     pygame.display.update()
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+            click = RECT1.collidepoint(pygame.mouse.get_pos())
+
+            if click == 1:
+                print ('CLICKED!')
+
+            click2 =
+
 
 pygame.quit()
