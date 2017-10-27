20379d8d6c31d014fcae835e0e4967159659294d
Subeen-Kim <subeen.kim@students.olin.edu>
2017-10-27 15:48:45 -0400

    new virus format with gorgeous time stuff

--- Country.py
+++ Country.py
@@ -6,17 +6,21 @@ RED = (255, 0, 0)
 
 pygame.init()
 
-
-class Ball:
-    def __init__(self, x, y, radius=50, color=RED, infected_pop=0):
+class Country:
+    def __init__(self, x, y, max_pop, radius=50, color=RED, infected_pop=0, infected_rate=1.1):
         self.initial_pos = (x, y)
         self.x = x
         self.y = y
         self.color = color
         self.radius = radius
         self.infected_pop = infected_pop
+        self.infected_rate = infected_rate
+        self.max_pop = max_pop
 
-    #def step
+    def step(self):
+        if self.infected_pop <= self.max_pop:
+            self.infected_pop = self.infected_pop * self.infected_rate
+            print(self.infected_pop)
 
     def draw(self):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)),
@@ -33,25 +37,32 @@ class Ball:
 
 screen = pygame.display.set_mode((640, 480))
 
-ball1 = Ball(320, 240, radius=80)
+country1 = Country(320, 240, 200, radius=80)
 
-balls = [ball1]
+countries = [country1]
+
+Time = 0
+clock = pygame.time.Clock()
 
 running = True
 while running:  # forever -- until user clicks in close box
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            for ball in balls:
-                if ball.contains_pt(pygame.mouse.get_pos()):
-                    if ball.infected_pop == 0:
-                        ball.infected_pop = ball.infected_pop + 1
-                        print(ball.infected_pop)
+            for country in countries:
+                if country.contains_pt(pygame.mouse.get_pos()):
+                    if country.infected_pop == 0:
+                        country.infected_pop = country.infected_pop + 1
+                        print(country.infected_pop)
+    if pygame.time.get_ticks() > (Time + 1000):
+        Time = pygame.time.get_ticks()
+        country.step()
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill(BLACK)  # erases screen
-    for ball in balls:
-        ball.draw()
+    for country in countries:
+        country.draw()
+
     pygame.display.update()  # updates real screen from staged screen
 
 pygame.quit()
